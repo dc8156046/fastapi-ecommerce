3efe990cc93475dc62274a6b03d6f2320831892f
Dan Chen <dan.chen01@edu.sait.ca>
2024-12-17 12:51:55 -0700

    fix database read settings bug

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.core import settings
+from app.core.settings import settings
 from sqlalchemy.ext.declarative import declarative_base
 
 # Database URL
