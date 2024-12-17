e6e0e60ed8107c95ac0c48f05d6958de75e8754d
Dan Chen <dan.chen01@edu.sait.ca>
2024-12-17 13:41:56 -0700

    update brands

--- app/api/v1/admin/brands.py
+++ app/api/v1/admin/brands.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends
-from deps import get_current_active_superuser
-from schemas.brand import BrandCreate, BrandDelete
+from schemas.brand import BrandCreate
 from deps import get_current_active_superuser, get_db
 from models.brand import Brand
 
