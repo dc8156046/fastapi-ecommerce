6f26712cf23780e26efba07035b6fb93dcd96c6b
Dan Chen <dan.chen01@edu.sait.ca>
2024-12-17 13:43:36 -0700

    fix products and brands api import error

--- app/api/v1/admin/brands.py
+++ app/api/v1/admin/brands.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from schemas.brand import BrandCreate
-from deps import get_current_active_superuser, get_db
+from app.api.deps import get_current_active_superuser, get_db
 from models.brand import Brand
 
 router = APIRouter()
--- app/api/v1/admin/products.py
+++ app/api/v1/admin/products.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from deps import get_current_active_superuser, get_db
+from app.api.deps import get_current_active_superuser, get_db
 from schemas.product import ProductCreate
 from models.product import Product
 
